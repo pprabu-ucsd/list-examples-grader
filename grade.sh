6fe9b9220a632abd3c8b199f16dbb02d05bdb302
pranavprabu22 <48165881+pranavprabu22@users.noreply.github.com>
2023-03-01 11:40:43 -0800

    Edited grade script

--- grade.sh
+++ grade.sh
@@ -1,6 +1,8 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm ListExamples.class
+rm TestListExamples.class
 git clone $1 student-submission
 echo 'Finished cloning'
 
@@ -8,31 +10,37 @@ cd student-submission
 
 if [[ -f ListExamples.java ]]
 then 
-    echo 'success'
+    echo 'FILE: EXISTS'
 else    
-    echo 'file does not exist'
+    echo 'Are you sure you passed in the right file?'
     exit
 fi
 
 cd ..
 
 
-cp student-submission/ListExamples.java .
+cp student-submission/ListExamples.java ./
 
 set +e
 
 javac -cp $CPATH *.java
 
-if [[ -f TestListExamples.class ]] && [[ -f ListExamples.class ]]
+if [[ -f TestListExamples.class ]] && [[ -f ListExamples.class ]] && [[ $(wc -l < grep_results.txt) -eq 0 ]]
 then
-    echo 'success 2'
+    echo 'COMPILE: SUCCESS'
 else
-    echo 'did not compile both TestListExamples and ListExamples properly'
+    echo 'You have a compile error. Check through your code again to see if there is a syntax error.'
     exit
 fi
 
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
 
-less output.txt
+grep -h 'FAILURES!!!' output.txt > grep_results.txt
 
 
+if [[ $(wc -l <grep_results.txt) -ge 1 ]]
+then
+    echo 'You have test failures. Fix them.'
+else
+    echo 'You passed all of your tests!'
+fi
